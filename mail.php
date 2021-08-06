249f855c09a2fff20ba16c0cdb3de86585ec3b35
martinaadimitrov <80766813+martinaadimitrov@users.noreply.github.com>
2021-08-06 11:04:24 +0200

    Create mail.php

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+//get data from form  
+{
+
+$name = $_POST['nameinput'];
+$email= $_POST['emailinput'];
+$message= $_POST['textareamessage'];
+$to = "martina_murdjovska@hotmail.com";
+$subject = "Mail From my website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+
+$headers = "From: martinamurdzovska@gmail.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("thankyou.html");
+?>
+}
